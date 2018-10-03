6bbcdceaa46a08e8ffc1c0b94aff457ba665368b
chris-gong <chris_gong@yahoo.com>
2018-10-02 23:56:57 -0400

    Uploaded initial project

---
+++ Assets/Scripts/FollowKnight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowKnight : MonoBehaviour {
+
+    public Transform knight;
+    public Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = knight.position + offset;
+	}
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    private Animator animator;
+    float vert;
+	// Use this for initialization
+	void Start () { 
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        vert = Input.GetAxis("Vertical");
+        animator.SetFloat("walkFloat", vert);
+        if (Input.GetKey(KeyCode.Space))
+        {
+            animator.SetBool("jumping", true);
+        }
+        else
+        {
+            animator.SetBool("jumping", false);
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            //Debug.Log("w is pressed");
+            animator.SetBool("runBool", true);
+        }
+        else
+        {
+            animator.SetBool("runBool", false);
+        }
+	}
+}
